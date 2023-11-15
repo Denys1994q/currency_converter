758959429f6e180ed131f73c16c4d0ec92103549
Denys1994q <drybachok@gmail.com>
2023-11-15 13:32:04 +0200

    before update angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'converter';
+    title = 'converter';
+    posts: any = []
+
+    constructor(private httpClient: HttpClient) {}
+
+    ngOnInit() {
+        this.httpClient.get('https://jsonplaceholder.typicode.com/posts').subscribe({
+            next: (data: any) => {
+                this.posts = data.slice(0,10);
+            }  
+        })
+    }
+
 }
